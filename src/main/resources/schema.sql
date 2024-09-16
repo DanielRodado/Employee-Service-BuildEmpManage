3c4199b89fffdc54c7a29e5bdd54824b5826ecc7
DanielRodado <d4nielrodado@gmail.com>
2024-09-15 20:38:53 -0500

    Esquema para la tabla 'Employees'

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS employees (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role VARCHAR(30) NOT NULL,
+    building VARCHAR(5) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
