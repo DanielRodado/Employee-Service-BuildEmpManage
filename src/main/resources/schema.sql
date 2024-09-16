313bfb73e4015446991f4f0d393f4032c3e863b0
DanielRodado <d4nielrodado@gmail.com>
2024-09-15 21:24:29 -0500

    Esquema para la tabla 'Employees' modificado

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS employees (
     name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     role VARCHAR(30) NOT NULL,
-    building VARCHAR(5) NOT NULL,
+    building VARCHAR(5),
     email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL
 );
