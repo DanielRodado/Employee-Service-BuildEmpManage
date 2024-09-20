9b116e487ab59e4a41b26abd6d155e09449e211f
DanielRodado <d4nielrodado@gmail.com>
2024-09-20 14:02:49 -0500

    Nueva excepción agregada al manejador global de excepciones

--- src/main/java/com/example/employee_service/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/employee_service/exceptions/GlobalExceptionHandler.java
@@ -9,6 +9,11 @@ import reactor.core.publisher.Mono;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(CustomServiceException.class)
+    public Mono<ResponseEntity<String>> handleCustomServiceException(CustomServiceException ex) {
+        return Mono.just(ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage()));
+    }
+
     @ExceptionHandler(EmployeeNotFoundException.class)
     public Mono<ResponseEntity<String>> handleEmployeeNotFoundException(EmployeeNotFoundException ex) {
         return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage()));
