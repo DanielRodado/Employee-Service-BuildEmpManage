df64194e5ea8b5957ee9b46312600af5d6c6077f
DanielRodado <d4nielrodado@gmail.com>
2024-09-20 16:51:26 -0500

    Nuevos mensajes (constantes)

--- src/main/java/com/example/employee_service/utils/Messages.java
+++ src/main/java/com/example/employee_service/utils/Messages.java
@@ -1,8 +1,10 @@
-package com.example.employee_service.configurations;
+package com.example.employee_service.utils;
 
 public final class Messages {
 
     public static final String EMPLOYEE_NOT_FOUND = "The employee was not found.";
     public static final String EMPLOYEE_UNASSIGNED = "The employee not assigned to building";
+    public static final String EMPLOYEE_ASSIGNED = "The employee assigned to building ";
+    public static final String EMPLOYEE_REMOVE = "The employee remove to building";
 
 }
