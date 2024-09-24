495cbaad1cd472d8d3757ae352213e7c4814cc97
DanielRodado <d4nielrodado@gmail.com>
2024-09-24 15:47:30 -0500

    Mapper de: App DTO -> Entity

--- src/main/java/com/example/employee_service/mapper/EmployeeMapper.java
+++ src/main/java/com/example/employee_service/mapper/EmployeeMapper.java
@@ -1,14 +1,24 @@
 package com.example.employee_service.mapper;
 
+import com.example.employee_service.dto.EmployeeApplicationDTO;
 import com.example.employee_service.dto.EmployeeDTO;
 import com.example.employee_service.model.EmployeeEntity;
 
 public class EmployeeMapper {
 
     // to entity dto
-
     public static EmployeeDTO toEmployeeDTO(EmployeeEntity employee) {
         return new EmployeeDTO(employee);
+    }
+
+    // from Application DTO to entity
+    public static EmployeeEntity toEmployeeEntity(EmployeeApplicationDTO employeeApplicationDTO) {
+        return EmployeeEntity.builder()
+                .name(employeeApplicationDTO.name())
+                .lastName(employeeApplicationDTO.lastName())
+                .email(employeeApplicationDTO.email())
+                .password(employeeApplicationDTO.password())
+                .build();
     }
 
 }
